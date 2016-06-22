d9d3237741f324b76d56dd2cff2bafdfd9be4436
Morshed Alam <morshed201@gmail.com>
2016-06-22 17:02:52 +0600

    Written method to create folder/project

--- lib/wrike3/common.rb
+++ lib/wrike3/common.rb
@@ -12,5 +12,15 @@ module Wrike3
       path = "#{type}/#{id}/#{path}" if type.present? && id.present?
       path
     end
+
+    def encode_params(parameters = {})
+      parameters.each do |item|
+        if item.last.is_a?(Hash) || item.last.is_a?(Array)
+          parameters[item.first.to_s] = item.last.to_json
+        end
+      end
+
+      parameters
+    end
   end
 end
--- lib/wrike3/folder.rb
+++ lib/wrike3/folder.rb
@@ -11,18 +11,23 @@ module Wrike3
     end
 
     # Get folder data
-    def details(ids, options={})
-      wrike.execute(:get, api_url("folders/#{ids}"), options)
+    def details(ids, params={})
+      wrike.execute(:get, api_url("folders/#{ids}"), encode_params(params))
+    end
+
+    # create a folder
+    def create(id, params = {})
+      wrike.execute(:post, api_url("/folders/#{id}/folders"), encode_params(params))
     end
 
     # Update a folder
-    def update(id, data = {}, options={})
-      wrike.execute(:put, api_url("folders/#{id}"), options.merge(data))
+    def update(id, params = {})
+      wrike.execute(:put, api_url("folders/#{id}"), encode_params(params))
     end
 
     # Delete folders and all descendants
-    def delete(id, options={})
-      wrike.execute(:delete, api_url("folders/#{id}"), options)
+    def delete(id, params={})
+      wrike.execute(:delete, api_url("folders/#{id}"), encode_params(params))
     end
 
   end
